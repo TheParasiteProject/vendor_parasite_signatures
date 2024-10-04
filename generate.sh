9c61011131caea071d097df5fc255baf14ca8005
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-10-05 01:05:53 +0900

    Run shfmt
    Change-Id: Iec8e369612eb98c986047349ded73d66c151a539

--- generate.sh
+++ generate.sh
@@ -9,116 +9,121 @@ CERTIFICATE_FILES_TXT="certificate-files.txt"
 
 # Check whether dir or symlink exists already
 if [ ! -d $OUTDIR ] || [ ! -f $OUTDIR ]; then
-    mkdir -p $OUTDIR
+	mkdir -p $OUTDIR
 fi
 
 if [ "$#" -gt 1 ] && [ "$#" -lt 3 ]; then
-    SKIP_PROMPT=true
-    echo 
+	SKIP_PROMPT=true
+	echo
 fi
 
 function confirm() {
-    while true; do
-        read -r -p "$1 (yes/no): " input
-        case "$input" in
-            [yY][eE][sS]|[yY]) echo "yes"; return ;;
-            [nN][oO]|[nN]) echo "no"; return ;;
-            *) ;;
-        esac
-    done
+	while true; do
+		read -r -p "$1 (yes/no): " input
+		case "$input" in
+		[yY][eE][sS] | [yY])
+			echo "yes"
+			return
+			;;
+		[nN][oO] | [nN])
+			echo "no"
+			return
+			;;
+		*) ;;
+		esac
+	done
 }
 
 function prompt_key_size() {
-    while true; do
-        read -p "$1" input
-        if [[ "$input" == "2048" || "$input" == "4096" ]]; then
-            echo "$input"
-            break
-        fi
-    done
+	while true; do
+		read -p "$1" input
+		if [[ "$input" == "2048" || "$input" == "4096" ]]; then
+			echo "$input"
+			break
+		fi
+	done
 }
 
 function prompt() {
-    while true; do
-        read -p "$1" input
-        if [[ -n "$input" ]]; then
-            echo "$input"
-            break
-        fi
-    done
+	while true; do
+		read -p "$1" input
+		if [[ -n "$input" ]]; then
+			echo "$input"
+			break
+		fi
+	done
 }
 
 function user_input() {
-    if [[ $(confirm "Do you want to customize the key size and subject?") == "yes" ]]; then
-        key_size=$(prompt_key_size "Enter the key size (2048 or 4096, APEX will always use 4096): ")
-        country_code=$(prompt "Enter the country code (e.g., US): ")
-        state=$(prompt "Enter the state or province (e.g., California): ")
-        city=$(prompt "Enter the city or locality (e.g., Mountain View): ")
-        org=$(prompt "Enter the organization (e.g., Android): ")
-        ou=$(prompt "Enter the organizational unit (e.g., Android): ")
-        cn=$(prompt "Enter the common name (e.g., Android): ")
-        email=$(prompt "Enter the email address (e.g., android@android.com): ")
+	if [[ $(confirm "Do you want to customize the key size and subject?") == "yes" ]]; then
+		key_size=$(prompt_key_size "Enter the key size (2048 or 4096, APEX will always use 4096): ")
+		country_code=$(prompt "Enter the country code (e.g., US): ")
+		state=$(prompt "Enter the state or province (e.g., California): ")
+		city=$(prompt "Enter the city or locality (e.g., Mountain View): ")
+		org=$(prompt "Enter the organization (e.g., Android): ")
+		ou=$(prompt "Enter the organizational unit (e.g., Android): ")
+		cn=$(prompt "Enter the common name (e.g., Android): ")
+		email=$(prompt "Enter the email address (e.g., android@android.com): ")
 
-        echo "Subject information to be used:"
-        echo "Key Size: $key_size"
-        echo "Country Code: $country_code"
-        echo "State/Province: $state"
-        echo "City/Locality: $city"
-        echo "Organization (O): $org"
-        echo "Organizational Unit (OU): $ou"
-        echo "Common Name (CN): $cn"
-        echo "Email Address: $email"
+		echo "Subject information to be used:"
+		echo "Key Size: $key_size"
+		echo "Country Code: $country_code"
+		echo "State/Province: $state"
+		echo "City/Locality: $city"
+		echo "Organization (O): $org"
+		echo "Organizational Unit (OU): $ou"
+		echo "Common Name (CN): $cn"
+		echo "Email Address: $email"
 
-        if [[ $(confirm "Is this information correct?") != "yes" ]]; then
-            echo "Generation aborted."
-            exit 0
-        fi
-    else
-        key_size='2048'
-        country_code='US'
-        state='California'
-        city='Mountain View'
-        org='Android'
-        ou='Android'
-        cn='Android'
-        email='android@android.com'
-    fi
+		if [[ $(confirm "Is this information correct?") != "yes" ]]; then
+			echo "Generation aborted."
+			exit 0
+		fi
+	else
+		key_size='2048'
+		country_code='US'
+		state='California'
+		city='Mountain View'
+		org='Android'
+		ou='Android'
+		cn='Android'
+		email='android@android.com'
+	fi
 
-    subject="/C=$country_code/ST=$state/L=$city/O=$org/OU=$ou/CN=$cn/emailAddress=$email"
+	subject="/C=$country_code/ST=$state/L=$city/O=$org/OU=$ou/CN=$cn/emailAddress=$email"
 }
 
 function generate_keys() {
-    for file in `cat $1`
-    do
-        if [[ (-f "$2/${file}.x509.pem" && -f "$2/${file}.pk8" && -f "$2/${file}-private.pem") ||
-              (-f "$2/${file}.certificate.override.x509.pem" && -f "$2/${file}.certificate.override.pk8" && -f "$2/${file}.certificate.override-private.pem") ]]; then
-            echo "$file already exists. Skipping..."
-        else
-            if [[ $file = *".override" ]]; then
-                bash $MAKEKEY "$2/$file" "$subject" 4096
-            else
-                bash $MAKEKEY "$2/$file" "$subject" $key_size
-            fi
-        fi
-    done
+	for file in $(cat $1); do
+		if [[ (-f "$2/${file}.x509.pem" && -f "$2/${file}.pk8" && -f "$2/${file}-private.pem") ||
+			(-f "$2/${file}.certificate.override.x509.pem" && -f "$2/${file}.certificate.override.pk8" && -f "$2/${file}.certificate.override-private.pem") ]]; then
+			echo "$file already exists. Skipping..."
+		else
+			if [[ $file = *".override" ]]; then
+				bash $MAKEKEY "$2/$file" "$subject" 4096
+			else
+				bash $MAKEKEY "$2/$file" "$subject" $key_size
+			fi
+		fi
+	done
 
-    if [[ ! -f "$2/avb_pkmd.bin" ]]; then
-        # Generate avb_pkmd.bin
-        $AVBTOOL extract_public_key \
-          --key "$2/releasekey-private.pem" \
-          --output "$2/avb_pkmd.bin"
-    fi
+	if [[ ! -f "$2/avb_pkmd.bin" ]]; then
+		# Generate avb_pkmd.bin
+		$AVBTOOL extract_public_key \
+			--key "$2/releasekey-private.pem" \
+			--output "$2/avb_pkmd.bin"
+	fi
 }
 
 if [[ $SKIP_PROMPT = true ]]; then
-    subject="$1"
-    if ! [[ $2 =~ ^[0-9]+$ ]]; then
-      key_size=2048
-    else
-      key_size="$2"
-    fi
+	subject="$1"
+	if ! [[ $2 =~ ^[0-9]+$ ]]; then
+		key_size=2048
+	else
+		key_size="$2"
+	fi
 else
-    user_input
+	user_input
 fi
 
 generate_keys $CERTIFICATE_FILES_TXT $OUTDIR
--- setup-buildfiles.sh
+++ setup-buildfiles.sh
@@ -17,90 +17,90 @@ PRODUCTMK="$OUT_DIR_ROOT/certificates.mk"
 CERTIFICATE_FILES_TXT="certificate-files.txt"
 
 function write_blueprint_header() {
-    if [ -f "$ANDROIDBP" ]; then
-        rm "$ANDROIDBP"
-    fi
+	if [ -f "$ANDROIDBP" ]; then
+		rm "$ANDROIDBP"
+	fi
 
-    cat << EOF >> "$ANDROIDBP"
+	cat <<EOF >>"$ANDROIDBP"
 // Automatically generated file. DO NOT MODIFY
 
 EOF
 }
 
 function write_blueprint_packages() {
-    local cert_dir_name=$2
+	local cert_dir_name=$2
 
-    local files=(`cat $1 | grep '.override'`)
-    for ((i=0; i<"${#files[@]}"; i++)); do
-        file_name="${files[$i]}"
-        printf 'android_app_certificate {\n' >> "$ANDROIDBP"
-        printf '\tname: "%s",\n' "$file_name" >> "$ANDROIDBP"
-        printf '\tcertificate: "%s",\n' "$cert_dir_name"/"$file_name" >> "$ANDROIDBP"
-        printf "}\n" >> "$ANDROIDBP"
-        if [ $i -lt $((${#files[@]} - 1)) ]; then
-            printf "\n" >> "$ANDROIDBP"
-        fi
-    done
+	local files=($(cat $1 | grep '.override'))
+	for ((i = 0; i < "${#files[@]}"; i++)); do
+		file_name="${files[$i]}"
+		printf 'android_app_certificate {\n' >>"$ANDROIDBP"
+		printf '\tname: "%s",\n' "$file_name" >>"$ANDROIDBP"
+		printf '\tcertificate: "%s",\n' "$cert_dir_name"/"$file_name" >>"$ANDROIDBP"
+		printf "}\n" >>"$ANDROIDBP"
+		if [ $i -lt $((${#files[@]} - 1)) ]; then
+			printf "\n" >>"$ANDROIDBP"
+		fi
+	done
 }
 
 function write_certificate_overrides_makefile_header() {
-    if [ -f "$PRODUCTMK" ]; then
-        rm "$PRODUCTMK"
-    fi
+	if [ -f "$PRODUCTMK" ]; then
+		rm "$PRODUCTMK"
+	fi
 
-    cat << EOF >> "$PRODUCTMK"
+	cat <<EOF >>"$PRODUCTMK"
 # Automatically generated file. DO NOT MODIFY
 
 EOF
 }
 
 function write_product_certificate_overrides() {
-    printf '%s\n' "PRODUCT_CERTIFICATE_OVERRIDES := \\" >> "$PRODUCTMK"
+	printf '%s\n' "PRODUCT_CERTIFICATE_OVERRIDES := \\" >>"$PRODUCTMK"
 
-    local files=(`cat $1 | grep '.override'`)
-    local file_name=
-    local file_to_overrides=
-    for ((i=0; i<"${#files[@]}"; i++)); do
-        file_name="${files[$i]}"
-        if [[ $file_name == *".certificate.override" ]]; then
-            file_to_overrides="${file_name%\.certificate\.override}"
-        else
-            file_to_overrides="${file_name%\.override}"
-        fi
-        printf '\t%s' "$file_to_overrides:$file_name" >> "$PRODUCTMK"
-        if [ $i -lt $((${#files[@]} - 1)) ]; then
-            printf '%s\n' " \\" >> "$PRODUCTMK"
-        else
-            printf '\n\n' >> "$PRODUCTMK"
-        fi
-    done
+	local files=($(cat $1 | grep '.override'))
+	local file_name=
+	local file_to_overrides=
+	for ((i = 0; i < "${#files[@]}"; i++)); do
+		file_name="${files[$i]}"
+		if [[ $file_name == *".certificate.override" ]]; then
+			file_to_overrides="${file_name%\.certificate\.override}"
+		else
+			file_to_overrides="${file_name%\.override}"
+		fi
+		printf '\t%s' "$file_to_overrides:$file_name" >>"$PRODUCTMK"
+		if [ $i -lt $((${#files[@]} - 1)) ]; then
+			printf '%s\n' " \\" >>"$PRODUCTMK"
+		else
+			printf '\n\n' >>"$PRODUCTMK"
+		fi
+	done
 
-    echo 'PRODUCT_DEFAULT_DEV_CERTIFICATE := $(CERTIFICATE_COMMON)/data/releasekey' >> "$PRODUCTMK"
-    echo 'PRODUCT_OTA_PUBLIC_KEYS := $(CERTIFICATE_COMMON)/data/releasekey.x509.pem' >> "$PRODUCTMK"
-    echo 'PRODUCT_EXTRA_RECOVERY_KEYS += $(CERTIFICATE_COMMON)/data/releasekey' >> "$PRODUCTMK"
+	echo 'PRODUCT_DEFAULT_DEV_CERTIFICATE := $(CERTIFICATE_COMMON)/data/releasekey' >>"$PRODUCTMK"
+	echo 'PRODUCT_OTA_PUBLIC_KEYS := $(CERTIFICATE_COMMON)/data/releasekey.x509.pem' >>"$PRODUCTMK"
+	echo 'PRODUCT_EXTRA_RECOVERY_KEYS += $(CERTIFICATE_COMMON)/data/releasekey' >>"$PRODUCTMK"
 }
 
 function create_symlinks() {
-    if [ -z $1 ] || [ -z $2 ]; then
-        return
-    fi
+	if [ -z $1 ] || [ -z $2 ]; then
+		return
+	fi
 
-    local source_file=$1
-    local dir_to_work=$2
-    local is_cert=$3
+	local source_file=$1
+	local dir_to_work=$2
+	local is_cert=$3
 
-    cd "$dir_to_work"
+	cd "$dir_to_work"
 
-    local target_file=`basename $source_file`
+	local target_file=$(basename $source_file)
 
-    if [[ $is_cert = true ]]; then
-        ln -fs $source_file.pk8 "$target_file".pk8
-        ln -fs $source_file.x509.pem "$target_file".x509.pem
-    else
-        ln -fs $source_file "$target_file"
-    fi
+	if [[ $is_cert = true ]]; then
+		ln -fs $source_file.pk8 "$target_file".pk8
+		ln -fs $source_file.x509.pem "$target_file".x509.pem
+	else
+		ln -fs $source_file "$target_file"
+	fi
 
-    cd $CWD
+	cd $CWD
 }
 
 write_blueprint_header
@@ -108,8 +108,8 @@ write_certificate_overrides_makefile_header
 write_blueprint_packages $CERTIFICATE_FILES_TXT $OUT
 write_product_certificate_overrides $CERTIFICATE_FILES_TXT
 
-for certs in `cat $CERTIFICATE_FILES_TXT`; do
-    create_symlinks "$PRIVATE_KEY_DIR/$certs" $OUTDIR true
+for certs in $(cat $CERTIFICATE_FILES_TXT); do
+	create_symlinks "$PRIVATE_KEY_DIR/$certs" $OUTDIR true
 done
 
 create_symlinks "$PRIVATE_KEY_DIR/avb_pkmd.bin" $OUTDIR
